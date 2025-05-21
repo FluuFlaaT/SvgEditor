0cae8cd2c97992da177bbee3284b455a8a9cb000
nereus.bai <byd2004@gmail.com>
2025-05-21 19:32:18 +0800

    feat(translations): add English and Chinese translations for UI elements

---
+++ translations/en_US.ts
@@ -0,0 +1,129 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>LeftSideBar</name>
+    <message>
+        <source>Drawing Tools</source>
+        <translation>Drawing Tools</translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation>Select</translation>
+    </message>
+    <message>
+        <source>Line</source>
+        <translation>Line</translation>
+    </message>
+    <message>
+        <source>Rectangle</source>
+        <translation>Rectangle</translation>
+    </message>
+    <message>
+        <source>Circle</source>
+        <translation>Circle</translation>
+    </message>
+    <message>
+        <source>Ellipse</source>
+        <translation>Ellipse</translation>
+    </message>
+    <message>
+        <source>Polyline</source>
+        <translation>Polyline</translation>
+    </message>
+    <message>
+        <source>Path</source>
+        <translation>Path</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>SVG Editor</source>
+        <translation>SVG Editor</translation>
+    </message>
+    <message>
+        <source>File</source>
+        <translation>File</translation>
+    </message>
+    <message>
+        <source>New</source>
+        <translation>New</translation>
+    </message>
+    <message>
+        <source>Open</source>
+        <translation>Open</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Save</translation>
+    </message>
+    <message>
+        <source>Save As...</source>
+        <translation>Save As...</translation>
+    </message>
+    <message>
+        <source>Export PNG...</source>
+        <translation>Export PNG...</translation>
+    </message>
+    <message>
+        <source>Exit</source>
+        <translation>Exit</translation>
+    </message>
+    <message>
+        <source>File Operations</source>
+        <translation>File Operations</translation>
+    </message>
+    <message>
+        <source>Ready</source>
+        <translation>Ready</translation>
+    </message>
+    <message>
+        <source>Open SVG File</source>
+        <translation>Open SVG File</translation>
+    </message>
+    <message>
+        <source>SVG Files (*.svg)</source>
+        <translation>SVG Files (*.svg)</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Error</translation>
+    </message>
+    <message>
+        <source>Cannot open file: </source>
+        <translation>Cannot open file: </translation>
+    </message>
+    <message>
+        <source>File saved: </source>
+        <translation>File saved: </translation>
+    </message>
+    <message>
+        <source>Cannot save file: </source>
+        <translation>Cannot save file: </translation>
+    </message>
+    <message>
+        <source>Save SVG File</source>
+        <translation>Save SVG File</translation>
+    </message>
+    <message>
+        <source>Information</source>
+        <translation>Information</translation>
+    </message>
+    <message>
+        <source>This feature will be available after drawing layer implementation is completed.</source>
+        <translation>This feature will be available after drawing layer implementation is completed.</translation>
+    </message>
+    <message>
+        <source>Untitled</source>
+        <translation>Untitled</translation>
+    </message>
+</context>
+<context>
+    <name>RightAttrBar</name>
+    <message>
+        <source>Properties Panel</source>
+        <translation>Properties Panel</translation>
+    </message>
+</context>
+</TS>
---
+++ translations/zh_CN.ts
@@ -0,0 +1,129 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>LeftSideBar</name>
+    <message>
+        <source>Drawing Tools</source>
+        <translation>绘图工具</translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation>选择</translation>
+    </message>
+    <message>
+        <source>Line</source>
+        <translation>直线</translation>
+    </message>
+    <message>
+        <source>Rectangle</source>
+        <translation>矩形</translation>
+    </message>
+    <message>
+        <source>Circle</source>
+        <translation>圆形</translation>
+    </message>
+    <message>
+        <source>Ellipse</source>
+        <translation>椭圆</translation>
+    </message>
+    <message>
+        <source>Polyline</source>
+        <translation>折线</translation>
+    </message>
+    <message>
+        <source>Path</source>
+        <translation>路径</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>SVG Editor</source>
+        <translation>SVG 编辑器</translation>
+    </message>
+    <message>
+        <source>File</source>
+        <translation>文件</translation>
+    </message>
+    <message>
+        <source>New</source>
+        <translation>新建</translation>
+    </message>
+    <message>
+        <source>Open</source>
+        <translation>打开</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>保存</translation>
+    </message>
+    <message>
+        <source>Save As...</source>
+        <translation>另存为...</translation>
+    </message>
+    <message>
+        <source>Export PNG...</source>
+        <translation>导出 PNG...</translation>
+    </message>
+    <message>
+        <source>Exit</source>
+        <translation>退出</translation>
+    </message>
+    <message>
+        <source>File Operations</source>
+        <translation>文件操作</translation>
+    </message>
+    <message>
+        <source>Ready</source>
+        <translation>就绪</translation>
+    </message>
+    <message>
+        <source>Open SVG File</source>
+        <translation>打开 SVG 文件</translation>
+    </message>
+    <message>
+        <source>SVG Files (*.svg)</source>
+        <translation>SVG 文件 (*.svg)</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>错误</translation>
+    </message>
+    <message>
+        <source>Cannot open file: </source>
+        <translation>无法打开文件: </translation>
+    </message>
+    <message>
+        <source>File saved: </source>
+        <translation>文件已保存: </translation>
+    </message>
+    <message>
+        <source>Cannot save file: </source>
+        <translation>无法保存文件: </translation>
+    </message>
+    <message>
+        <source>Save SVG File</source>
+        <translation>保存 SVG 文件</translation>
+    </message>
+    <message>
+        <source>Information</source>
+        <translation>信息</translation>
+    </message>
+    <message>
+        <source>This feature will be available after drawing layer implementation is completed.</source>
+        <translation>此功能将在绘图层实现完成后可用。</translation>
+    </message>
+    <message>
+        <source>Untitled</source>
+        <translation>未命名</translation>
+    </message>
+</context>
+<context>
+    <name>RightAttrBar</name>
+    <message>
+        <source>Properties Panel</source>
+        <translation>属性面板</translation>
+    </message>
+</context>
+</TS>
