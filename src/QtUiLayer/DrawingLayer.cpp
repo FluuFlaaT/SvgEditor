f15267713719bb01cf235df3ad5eac1277dc8c09
nereus.bai <byd2004@gmail.com>
2025-05-22 00:54:09 +0800

    feat(ui): add constructor and methods for enhanced DrawingLayer functionality

--- src/QtUiLayer/DrawingLayer.cpp
+++ src/QtUiLayer/DrawingLayer.cpp
@@ -19,6 +19,15 @@ DrawingLayer::DrawingLayer(QWidget *parent)
     LoggingService::getInstance().debug("DrawingLayer constructed");
 }
 
+DrawingLayer::DrawingLayer(CoreSvgEngine* engine, CommandManager* commandManager, QWidget *parent)
+    : QGraphicsView(parent), m_svgEngine(engine), m_commandManager(commandManager), m_currentDocument(nullptr), m_currentToolType(ToolId::TOOL_SELECT), m_isDragging(false) {
+    m_scene = new QGraphicsScene(this);
+    setScene(m_scene);
+    setupSceneDefaults();
+    // TODO: Connect signals from commandManager if necessary
+    LoggingService::getInstance().debug("DrawingLayer constructed with CoreSvgEngine and CommandManager");
+}
+
 DrawingLayer::~DrawingLayer()
 {
     LoggingService::getInstance().debug("DrawingLayer destroyed");
@@ -63,4 +72,283 @@ void DrawingLayer::changeEvent(QEvent* event)
     }
     
     QGraphicsView::changeEvent(event);
+}
+
+QColor DrawingLayer::toQColor(const Color& coreColor) const {
+    // TODO: Implement conversion from CoreSvg::Color to QColor
+    return QColor(); // Placeholder
+}
+
+Color DrawingLayer::toCoreColor(const QColor& qColor) const {
+    // TODO: Implement conversion from QColor to CoreSvg::Color
+    return Color(); // Placeholder
+}
+
+void DrawingLayer::loadDocument(SvgDocument* document) {
+    // TODO: Implement
+}
+
+void DrawingLayer::clearCanvasAndMappings() {
+    // TODO: Implement
+}
+
+void DrawingLayer::setCurrentToolType(ToolId toolType) {
+    m_currentToolType = toolType;
+    // TODO: Add any other necessary logic when tool type changes
+}
+
+ToolId DrawingLayer::getCurrentToolType() const {
+    return m_currentToolType;
+}
+
+void DrawingLayer::setZoomLevel(double level) {
+    // TODO: Implement
+}
+
+double DrawingLayer::getZoomLevel() const {
+    // TODO: Implement
+    return 1.0; // Placeholder
+}
+
+void DrawingLayer::fitCanvasToView() {
+    // TODO: Implement
+}
+
+void DrawingLayer::ensureVisible(const QRectF& rect, int xmargin, int ymargin) {
+    // TODO: Implement
+}
+
+// Slots to React to SvgDocument Changes
+void DrawingLayer::handleElementAddedToDocument(SvgElement* element) {
+    // TODO: Implement
+}
+
+void DrawingLayer::handleElementRemovedFromDocument(const QString& elementId) {
+    // TODO: Implement
+}
+
+void DrawingLayer::handleElementModifiedInDocument(SvgElement* element) {
+    // TODO: Implement
+}
+
+void DrawingLayer::handleDocumentCleared() {
+    // TODO: Implement
+}
+
+void DrawingLayer::handleDocumentDimensionsChanged(double width, double height) {
+    // TODO: Implement
+}
+
+void DrawingLayer::handleDocumentBackgroundColorChanged(const Color& coreColor) {
+    // TODO: Implement
+}
+
+// Public Slots for Actions
+void DrawingLayer::triggerSelectAll() {
+    // TODO: Implement
+}
+
+void DrawingLayer::triggerDeleteSelected() {
+    // TODO: Implement
+}
+
+void DrawingLayer::triggerCopySelected() {
+    // TODO: Implement
+}
+
+void DrawingLayer::triggerPaste() {
+    // TODO: Implement
+}
+
+void DrawingLayer::triggerBringToFront() {
+    // TODO: Implement
+}
+
+void DrawingLayer::triggerSendToBack() {
+    // TODO: Implement
+}
+
+void DrawingLayer::triggerMoveForward() {
+    // TODO: Implement
+}
+
+void DrawingLayer::triggerMoveBackward() {
+    // TODO: Implement
+}
+
+// Protected Event Handlers
+void DrawingLayer::mousePressEvent(QMouseEvent *event) {
+    // TODO: Implement
+    QGraphicsView::mousePressEvent(event);
+}
+
+void DrawingLayer::mouseMoveEvent(QMouseEvent *event) {
+    // TODO: Implement
+    QGraphicsView::mouseMoveEvent(event);
+}
+
+void DrawingLayer::mouseReleaseEvent(QMouseEvent *event) {
+    // TODO: Implement
+    QGraphicsView::mouseReleaseEvent(event);
+}
+
+void DrawingLayer::wheelEvent(QWheelEvent *event) {
+    // TODO: Implement
+    QGraphicsView::wheelEvent(event);
+}
+
+void DrawingLayer::contextMenuEvent(QContextMenuEvent *event) {
+    // TODO: Implement
+    QGraphicsView::contextMenuEvent(event);
+}
+
+void DrawingLayer::keyPressEvent(QKeyEvent *event) {
+    // TODO: Implement
+    QGraphicsView::keyPressEvent(event);
+}
+
+void DrawingLayer::resizeEvent(QResizeEvent *event) {
+    // TODO: Implement
+    QGraphicsView::resizeEvent(event);
+}
+
+void DrawingLayer::showEvent(QShowEvent* event) {
+    // TODO: Implement
+    QGraphicsView::showEvent(event);
+}
+
+void DrawingLayer::drawBackground(QPainter *painter, const QRectF &rect) {
+    // TODO: Implement
+    QGraphicsView::drawBackground(painter, rect);
+}
+
+// Private Helper Methods
+void DrawingLayer::setupSceneDefaults() {
+    // TODO: Implement
+}
+
+void DrawingLayer::connectDocumentSignals(SvgDocument* doc) {
+    // TODO: Implement
+}
+
+void DrawingLayer::disconnectDocumentSignals(SvgDocument* doc) {
+    // TODO: Implement
+}
+
+QGraphicsItem* DrawingLayer::createAndAddItemForElement(SvgElement* element) {
+    // TODO: Implement
+    return nullptr; // Placeholder
+}
+
+void DrawingLayer::updateItemFromElement(QGraphicsItem* item, SvgElement* element) {
+    // TODO: Implement
+}
+
+void DrawingLayer::removeItemAndMapping(const QString& elementId) {
+    // TODO: Implement
+}
+
+void DrawingLayer::updateAllItemsFromDocument() {
+    // TODO: Implement
+}
+
+void DrawingLayer::createContextMenu(QMenu& menu, QGraphicsItem* clickedItem) {
+    // TODO: Implement
+}
+
+void DrawingLayer::dispatchMousePressEvent(QMouseEvent* event) {
+    // TODO: Implement
+}
+
+void DrawingLayer::dispatchMouseMoveEvent(QMouseEvent* event) {
+    // TODO: Implement
+}
+
+void DrawingLayer::dispatchMouseReleaseEvent(QMouseEvent* event) {
+    // TODO: Implement
+}
+
+// Specific tool interaction handlers
+void DrawingLayer::handleToolPress_Selection(const QPointF& scenePos, Qt::KeyboardModifiers modifiers) {
+    // TODO: Implement
+}
+
+void DrawingLayer::handleToolMove_Selection(const QPointF& scenePos, Qt::KeyboardModifiers modifiers) {
+    // TODO: Implement
+}
+
+void DrawingLayer::handleToolRelease_Selection(const QPointF& scenePos, Qt::KeyboardModifiers modifiers) {
+    // TODO: Implement
+}
+
+void DrawingLayer::handleToolPress_Rectangle(const QPointF& scenePos, Qt::KeyboardModifiers modifiers) {
+    // TODO: Implement
+}
+
+void DrawingLayer::handleToolMove_Rectangle(const QPointF& scenePos, Qt::KeyboardModifiers modifiers) {
+    // TODO: Implement
+}
+
+void DrawingLayer::handleToolRelease_Rectangle(const QPointF& scenePos, Qt::KeyboardModifiers modifiers) {
+    // TODO: Implement
+}
+
+void DrawingLayer::handleToolPress_Line(const QPointF& scenePos, Qt::KeyboardModifiers modifiers) {
+    // TODO: Implement
+}
+
+void DrawingLayer::handleToolMove_Line(const QPointF& scenePos, Qt::KeyboardModifiers modifiers) {
+    // TODO: Implement
+}
+
+void DrawingLayer::handleToolRelease_Line(const QPointF& scenePos, Qt::KeyboardModifiers modifiers) {
+    // TODO: Implement
+}
+
+void DrawingLayer::handleToolPress_Circle(const QPointF& scenePos, Qt::KeyboardModifiers modifiers) {
+    // TODO: Implement
+}
+
+void DrawingLayer::handleToolMove_Circle(const QPointF& scenePos, Qt::KeyboardModifiers modifiers) {
+    // TODO: Implement
+}
+
+void DrawingLayer::handleToolRelease_Circle(const QPointF& scenePos, Qt::KeyboardModifiers modifiers) {
+    // TODO: Implement
+}
+
+void DrawingLayer::handleToolPress_Ellipse(const QPointF& scenePos, Qt::KeyboardModifiers modifiers) {
+    // TODO: Implement
+}
+
+void DrawingLayer::handleToolMove_Ellipse(const QPointF& scenePos, Qt::KeyboardModifiers modifiers) {
+    // TODO: Implement
+}
+
+void DrawingLayer::handleToolRelease_Ellipse(const QPointF& scenePos, Qt::KeyboardModifiers modifiers) {
+    // TODO: Implement
+}
+
+void DrawingLayer::handleToolPress_Polyline(const QPointF& scenePos, Qt::KeyboardModifiers modifiers) {
+    // TODO: Implement
+}
+
+void DrawingLayer::handleToolMove_Polyline(const QPointF& scenePos, Qt::KeyboardModifiers modifiers) {
+    // TODO: Implement
+}
+
+void DrawingLayer::handleToolRelease_Polyline(const QPointF& scenePos, Qt::KeyboardModifiers modifiers) {
+    // TODO: Implement
+}
+
+void DrawingLayer::handleToolPress_Path(const QPointF& scenePos, Qt::KeyboardModifiers modifiers) {
+    // TODO: Implement
+}
+
+void DrawingLayer::handleToolMove_Path(const QPointF& scenePos, Qt::KeyboardModifiers modifiers) {
+    // TODO: Implement
+}
+
+void DrawingLayer::handleToolRelease_Path(const QPointF& scenePos, Qt::KeyboardModifiers modifiers) {
+    // TODO: Implement
 }
