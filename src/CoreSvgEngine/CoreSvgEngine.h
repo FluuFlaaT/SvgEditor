d6109c6c4657c5ab98237d02cb8fe1691fa40566
nereus.bai <byd2004@gmail.com>
2025-05-23 06:52:08 +0800

    fix: change CoreSvgEngine library type from STATIC to SHARED and update link in SvgEditor

--- src/CoreSvgEngine/CoreSvgEngine.h
+++ src/CoreSvgEngine/CoreSvgEngine.h
@@ -2,7 +2,21 @@
 #include "SvgDocument.h"
 #include <memory>
 
-class CoreSvgEngine {
+#if defined(_MSC_VER) || defined(WIN64) || defined(_WIN64) || defined(__WIN64__) || defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+#  define DECL_EXPORT __declspec(dllexport)
+#  define DECL_IMPORT __declspec(dllimport)
+#else
+#  define DECL_EXPORT     __attribute__((visibility("default")))
+#  define DECL_IMPORT     __attribute__((visibility("default")))
+#endif
+
+#if defined(CORESVGENGINE_LIBRARY)
+#  define CORESVGENGINE_EXPORT DECL_EXPORT
+#else
+#  define CORESVGENGINE_EXPORT DECL_IMPORT
+#endif
+
+class CORESVGENGINE_EXPORT CoreSvgEngine {
 private:
     std::unique_ptr<SvgDocument> m_document; // 唯一的文档指针
 
