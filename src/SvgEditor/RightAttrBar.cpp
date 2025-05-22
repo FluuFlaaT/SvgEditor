a92e7d7b07d7378fb6b3e97ff221bb313cbd21b5
nereus.bai <byd2004@gmail.com>
2025-05-23 03:10:43 +0800

    feat(RightAttrBar): add methods for updating canvas size and color, and manage widget types

--- src/SvgEditor/RightAttrBar.cpp
+++ src/SvgEditor/RightAttrBar.cpp
@@ -3,9 +3,12 @@
 Q_LOGGING_CATEGORY(rightAttrBarLog, "RightAttrBar")
 
 RightAttrBar::RightAttrBar(QWidget *parent)
-    : QWidget(parent)
+    : QWidget(parent),
+    m_canvasColor(Qt::white),
+    m_borderColor(Qt::black),
+    m_fillColor(Qt::white)
 {
-    // Widget initialization placeholder
+    // Widget initialization
     setFixedWidth(300);
 
     m_mainLayout = new QVBoxLayout(this);
@@ -14,10 +17,15 @@ RightAttrBar::RightAttrBar(QWidget *parent)
 
     m_stackedWidget = new QStackedWidget(this);
 
-    // m_stackedWidget->addWidget(createCommonAttributesWidget());
+    // Add all widget types to the stacked widget
+    m_stackedWidget->addWidget(createCommonAttributesWidget());
     m_stackedWidget->addWidget(createCircleAttributesWidget());
+    // Uncomment when implemented
     // m_stackedWidget->addWidget(createRectangleAttributesWidget());
     // m_stackedWidget->addWidget(createLineAttributesWidget());
+
+    // Set the common attributes widget as the default
+    m_stackedWidget->setCurrentIndex(CommonAttributes);
 
     m_mainLayout->addWidget(m_stackedWidget);
     m_mainLayout->addStretch();
@@ -30,65 +38,129 @@ RightAttrBar::~RightAttrBar()
     // Cleanup placeholder
 }
 
+void RightAttrBar::setCurrentWidget(int widgetType)
+{
+    if (widgetType >= 0 && widgetType < m_stackedWidget->count()) {
+        m_stackedWidget->setCurrentIndex(widgetType);
+        qCDebug(rightAttrBarLog) << "Switched to widget type:" << widgetType;
+    } else {
+        qCWarning(rightAttrBarLog) << "Invalid widget type:" << widgetType;
+    }
+}
+
+void RightAttrBar::updateCanvasSize(int width, int height)
+{
+    // Only update if the values have actually changed
+    if (m_canvasWidthSpinBox->value() != width || m_canvasHeightSpinBox->value() != height) {
+        // Update the spinbox values without triggering signals
+        m_canvasWidthSpinBox->blockSignals(true);
+        m_canvasHeightSpinBox->blockSignals(true);
+
+        m_canvasWidthSpinBox->setValue(width);
+        m_canvasHeightSpinBox->setValue(height);
+
+        m_canvasWidthSpinBox->blockSignals(false);
+        m_canvasHeightSpinBox->blockSignals(false);
+
+        qCDebug(rightAttrBarLog) << "Canvas size updated to:" << width << "x" << height;
+    }
+}
+
+void RightAttrBar::updateCanvasColor(const QColor& color)
+{
+    if (color.isValid()) {
+        // Only update if the color has actually changed
+        if (m_canvasColor != color) {
+            m_canvasColor = color;
+            QString qss = QString("background-color: %1").arg(color.name());
+            m_canvasColorButton->setStyleSheet(qss);
+            qCDebug(rightAttrBarLog) << "Canvas color updated to:" << color.name()
+                                    << "RGBA:" << color.red() << color.green() << color.blue() << color.alpha();
+        }
+    }
+}
+
+void RightAttrBar::updateZoomLevel(qreal zoomFactor)
+{
+    if (m_zoomEdit) {
+        m_zoomEdit->setText(tr("%1%").arg(int(zoomFactor * 100)));
+        qCDebug(rightAttrBarLog) << "Zoom level updated to:" << zoomFactor;
+    }
+}
+
 QWidget* RightAttrBar::createCommonAttributesWidget() {
     QWidget* widget = new QWidget();
     QVBoxLayout* layout = new QVBoxLayout(widget);
     layout->setContentsMargins(5, 10, 5, 10);
     layout->setSpacing(10);
-    
-    // 1. 画布宽度
+
+    // 1. Canvas width
     QHBoxLayout* widthLayout = new QHBoxLayout();
     QLabel* widthLabel = new QLabel(tr("Canvas Width:"));
-    QSpinBox* widthSpinBox = new QSpinBox();
-    widthSpinBox->setRange(1, 9999);
-    widthSpinBox->setValue(800); // Default value
-    widthSpinBox->setSuffix(" px");
+    m_canvasWidthSpinBox = new QSpinBox();
+    m_canvasWidthSpinBox->setRange(1, 9999);
+    m_canvasWidthSpinBox->setValue(800); // Default value
+    m_canvasWidthSpinBox->setSuffix(" px");
     widthLayout->addWidget(widthLabel);
-    widthLayout->addWidget(widthSpinBox);
-    
-    // 2. 画布高度
+    widthLayout->addWidget(m_canvasWidthSpinBox);
+
+    // 2. Canvas height
     QHBoxLayout* heightLayout = new QHBoxLayout();
     QLabel* heightLabel = new QLabel(tr("Canvas Height:"));
-    QSpinBox* heightSpinBox = new QSpinBox();
-    heightSpinBox->setRange(1, 9999);
-    heightSpinBox->setValue(600); // Default value
-    heightSpinBox->setSuffix(" px");
+    m_canvasHeightSpinBox = new QSpinBox();
+    m_canvasHeightSpinBox->setRange(1, 9999);
+    m_canvasHeightSpinBox->setValue(600); // Default value
+    m_canvasHeightSpinBox->setSuffix(" px");
     heightLayout->addWidget(heightLabel);
-    heightLayout->addWidget(heightSpinBox);
-    
-    // 3. 缩放比例（不可编辑）
+    heightLayout->addWidget(m_canvasHeightSpinBox);
+
+    // 3. Zoom level (read-only)
     QHBoxLayout* zoomLayout = new QHBoxLayout();
     QLabel* zoomLabel = new QLabel(tr("Zoom:"));
-    QLineEdit* zoomEdit = new QLineEdit("100%");
-    zoomEdit->setReadOnly(true);
+    m_zoomEdit = new QLineEdit("100%");
+    m_zoomEdit->setReadOnly(true);
     zoomLayout->addWidget(zoomLabel);
-    zoomLayout->addWidget(zoomEdit);
-    
-    // 4. 画布颜色（QColorDialog）
+    zoomLayout->addWidget(m_zoomEdit);
+
+    // 4. Canvas color
     QHBoxLayout* colorLayout = new QHBoxLayout();
     QLabel* colorLabel = new QLabel(tr("Canvas Color:"));
-    QPushButton* colorButton = new QPushButton();
-    colorButton->setFixedSize(30, 30);
-    QColor initialColor(Qt::white);
-    QString qss = QString("background-color: %1").arg(initialColor.name());
-    colorButton->setStyleSheet(qss);
-    connect(colorButton, &QPushButton::clicked, this, [=]() {
-        QColor color = QColorDialog::getColor(initialColor, this, tr("Select Canvas Color"));
+    m_canvasColorButton = new QPushButton();
+    m_canvasColorButton->setFixedSize(30, 30);
+    QString qss = QString("background-color: %1").arg(m_canvasColor.name());
+    m_canvasColorButton->setStyleSheet(qss);
+
+    // Connect color button click to color dialog
+    connect(m_canvasColorButton, &QPushButton::clicked, this, [this]() {
+        QColor color = QColorDialog::getColor(m_canvasColor, this, tr("Select Canvas Color"));
         if (color.isValid()) {
+            m_canvasColor = color;
             QString qss = QString("background-color: %1").arg(color.name());
-            colorButton->setStyleSheet(qss);
-            // Emit signal or call function to change canvas color
+            m_canvasColorButton->setStyleSheet(qss);
+
+            // Emit signal to notify about color change
+            emit canvasColorChanged(color);
         }
     });
+
     colorLayout->addWidget(colorLabel);
-    colorLayout->addWidget(colorButton);
-    
+    colorLayout->addWidget(m_canvasColorButton);
+
+    // Connect size spinboxes to emit signals when values change
+    connect(m_canvasWidthSpinBox, QOverload<int>::of(&QSpinBox::valueChanged), this, [this](int width) {
+        emit canvasSizeChanged(width, m_canvasHeightSpinBox->value());
+    });
+
+    connect(m_canvasHeightSpinBox, QOverload<int>::of(&QSpinBox::valueChanged), this, [this](int height) {
+        emit canvasSizeChanged(m_canvasWidthSpinBox->value(), height);
+    });
+
     layout->addLayout(widthLayout);
     layout->addLayout(heightLayout);
     layout->addLayout(zoomLayout);
     layout->addLayout(colorLayout);
     layout->addStretch();
-    
+
     return widget;
 }
 
@@ -97,72 +169,76 @@ QWidget* RightAttrBar::createCircleAttributesWidget() {
     QVBoxLayout* layout = new QVBoxLayout(widget);
     layout->setContentsMargins(5, 10, 5, 10);
     layout->setSpacing(10);
-    
-    // 1. 边框宽度
+
+    // 1. Border width
     QHBoxLayout* borderWidthLayout = new QHBoxLayout();
     QLabel* borderWidthLabel = new QLabel(tr("Border Width:"));
-    QSpinBox* borderWidthSpinBox = new QSpinBox();
-    borderWidthSpinBox->setRange(0, 9);
-    borderWidthSpinBox->setValue(1); // Default value
-    borderWidthSpinBox->setSuffix(" px");
+    m_borderWidthSpinBox = new QSpinBox();
+    m_borderWidthSpinBox->setRange(0, 9);
+    m_borderWidthSpinBox->setValue(1); // Default value
+    m_borderWidthSpinBox->setSuffix(" px");
     borderWidthLayout->addWidget(borderWidthLabel);
-    borderWidthLayout->addWidget(borderWidthSpinBox);
-    
-    // 2. 边框样式
+    borderWidthLayout->addWidget(m_borderWidthSpinBox);
+
+    // 2. Border style
     QHBoxLayout* borderStyleLayout = new QHBoxLayout();
     QLabel* borderStyleLabel = new QLabel(tr("Border Style:"));
-    QComboBox* borderStyleComboBox = new QComboBox();
-    borderStyleComboBox->addItem(tr("Solid Line"), static_cast<int>(Qt::SolidLine));
-    borderStyleComboBox->addItem(tr("Dash Line"), static_cast<int>(Qt::DashLine));
-    borderStyleComboBox->addItem(tr("Dot Line"), static_cast<int>(Qt::DotLine));
-    borderStyleComboBox->addItem(tr("Dash Dot Line"), static_cast<int>(Qt::DashDotLine));
-    borderStyleComboBox->addItem(tr("Dash Dot Dot Line"), static_cast<int>(Qt::DashDotDotLine));
+    m_borderStyleComboBox = new QComboBox();
+    m_borderStyleComboBox->addItem(tr("Solid Line"), static_cast<int>(Qt::SolidLine));
+    m_borderStyleComboBox->addItem(tr("Dash Line"), static_cast<int>(Qt::DashLine));
+    m_borderStyleComboBox->addItem(tr("Dot Line"), static_cast<int>(Qt::DotLine));
+    m_borderStyleComboBox->addItem(tr("Dash Dot Line"), static_cast<int>(Qt::DashDotLine));
+    m_borderStyleComboBox->addItem(tr("Dash Dot Dot Line"), static_cast<int>(Qt::DashDotDotLine));
     borderStyleLayout->addWidget(borderStyleLabel);
-    borderStyleLayout->addWidget(borderStyleComboBox);
-    
-    // 3. 边框颜色
+    borderStyleLayout->addWidget(m_borderStyleComboBox);
+
+    // 3. Border color
     QHBoxLayout* borderColorLayout = new QHBoxLayout();
     QLabel* borderColorLabel = new QLabel(tr("Border Color:"));
-    QPushButton* borderColorButton = new QPushButton();
-    borderColorButton->setFixedSize(30, 30);
-    QColor initialBorderColor(Qt::black);
-    QString borderQss = QString("background-color: %1").arg(initialBorderColor.name());
-    borderColorButton->setStyleSheet(borderQss);
-    connect(borderColorButton, &QPushButton::clicked, this, [=]() {
-        QColor color = QColorDialog::getColor(initialBorderColor, this, tr("Select Border Color"));
+    m_borderColorButton = new QPushButton();
+    m_borderColorButton->setFixedSize(30, 30);
+    QString borderQss = QString("background-color: %1").arg(m_borderColor.name());
+    m_borderColorButton->setStyleSheet(borderQss);
+
+    connect(m_borderColorButton, &QPushButton::clicked, this, [this]() {
+        QColor color = QColorDialog::getColor(m_borderColor, this, tr("Select Border Color"));
         if (color.isValid()) {
+            m_borderColor = color;
             QString qss = QString("background-color: %1").arg(color.name());
-            borderColorButton->setStyleSheet(qss);
-            // Emit signal or call function to change border color
+            m_borderColorButton->setStyleSheet(qss);
+            // TODO: Emit signal for border color change
         }
     });
+
     borderColorLayout->addWidget(borderColorLabel);
-    borderColorLayout->addWidget(borderColorButton);
-    
-    // 4. 填充颜色
+    borderColorLayout->addWidget(m_borderColorButton);
+
+    // 4. Fill color
     QHBoxLayout* fillColorLayout = new QHBoxLayout();
     QLabel* fillColorLabel = new QLabel(tr("Fill Color:"));
-    QPushButton* fillColorButton = new QPushButton();
-    fillColorButton->setFixedSize(30, 30);
-    QColor initialFillColor(Qt::white);
-    QString fillQss = QString("background-color: %1").arg(initialFillColor.name());
-    fillColorButton->setStyleSheet(fillQss);
-    connect(fillColorButton, &QPushButton::clicked, this, [=]() {
-        QColor color = QColorDialog::getColor(initialFillColor, this, tr("Select Fill Color"));
+    m_fillColorButton = new QPushButton();
+    m_fillColorButton->setFixedSize(30, 30);
+    QString fillQss = QString("background-color: %1").arg(m_fillColor.name());
+    m_fillColorButton->setStyleSheet(fillQss);
+
+    connect(m_fillColorButton, &QPushButton::clicked, this, [this]() {
+        QColor color = QColorDialog::getColor(m_fillColor, this, tr("Select Fill Color"));
         if (color.isValid()) {
+            m_fillColor = color;
             QString qss = QString("background-color: %1").arg(color.name());
-            fillColorButton->setStyleSheet(qss);
-            // Emit signal or call function to change fill color
+            m_fillColorButton->setStyleSheet(qss);
+            // TODO: Emit signal for fill color change
         }
     });
+
     fillColorLayout->addWidget(fillColorLabel);
-    fillColorLayout->addWidget(fillColorButton);
-    
+    fillColorLayout->addWidget(m_fillColorButton);
+
     layout->addLayout(borderWidthLayout);
     layout->addLayout(borderStyleLayout);
     layout->addLayout(borderColorLayout);
     layout->addLayout(fillColorLayout);
     layout->addStretch();
-    
+
     return widget;
 }
--- src/SvgEditor/RightAttrBar.h
+++ src/SvgEditor/RightAttrBar.h
@@ -23,12 +23,47 @@ public:
     explicit RightAttrBar(QWidget *parent = nullptr);
     ~RightAttrBar();
 
+    // Enum for widget types
+    enum WidgetType {
+        CommonAttributes = 0,
+        CircleAttributes,
+        RectangleAttributes,
+        LineAttributes
+    };
+
 public slots:
-    // Add future slots here
+    void setCurrentWidget(int widgetType);
+    void updateCanvasSize(int width, int height);
+    void updateCanvasColor(const QColor& color);
+    void updateZoomLevel(qreal zoomFactor);
+
+public:
+    QColor getCanvasColor() const { return m_canvasColor; }
+    int getCanvasWidth() const { return m_canvasWidthSpinBox ? m_canvasWidthSpinBox->value() : 800; }
+    int getCanvasHeight() const { return m_canvasHeightSpinBox ? m_canvasHeightSpinBox->value() : 600; }
+
+signals:
+    void canvasSizeChanged(int width, int height);
+    void canvasColorChanged(const QColor& color);
 
 private:
     QVBoxLayout* m_mainLayout;
     QStackedWidget* m_stackedWidget;
+
+    // Common attributes controls
+    QSpinBox* m_canvasWidthSpinBox;
+    QSpinBox* m_canvasHeightSpinBox;
+    QLineEdit* m_zoomEdit;
+    QPushButton* m_canvasColorButton;
+    QColor m_canvasColor;
+
+    // Circle attributes controls
+    QSpinBox* m_borderWidthSpinBox;
+    QComboBox* m_borderStyleComboBox;
+    QPushButton* m_borderColorButton;
+    QPushButton* m_fillColorButton;
+    QColor m_borderColor;
+    QColor m_fillColor;
 
     QWidget* createCommonAttributesWidget();
     QWidget* createCircleAttributesWidget();
